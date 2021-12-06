9964516731b1b2cdc4e085ec297c184b48e53dd9
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-12-06 14:06:39 +0900

    メモを追加

--- app/src/main/java/com/example/balltouchapp/MainActivity.kt
+++ app/src/main/java/com/example/balltouchapp/MainActivity.kt
@@ -13,18 +13,20 @@ import android.view.View
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //XMLを使わないのでコメントアウト
+        //XMLを使わないのでコメントアウト↓
         //setContentView(R.layout.activity_main)
 
-        //XMLではなく、ballViewを表示する
+        //XMLではなく、↓のBallViewを表示する
         val ballView = BallView(this)
         setContentView(ballView)
     }
-    //classクラス名:継承元のクラス名　にcontext、(引数)を追加
+
+    //class　クラス名:()継承元のクラス名()　にcontext、(引数)を追加
+    //すると親のメソッドを使うことが出来る。
     class BallView(context: Context?) : View(context) {
         //paint：図形の描画
         private var paint: Paint = Paint()
-        //縦横
+        //縦横、Float型
         private var circleX:Float = 200F
         private var circleY:Float = 200F
 
@@ -32,16 +34,19 @@ class MainActivity : AppCompatActivity() {
         override fun onDraw(canvas: Canvas?) {
             super.onDraw(canvas)
 
-            canvas?.drawColor(Color.RED)//カンバスの色
+            canvas?.drawColor(Color.RED)//カンバスの色、null許容なので
             //色と丸い図形
             paint.color = Color.YELLOW
+            //たて、よこ、半径radius
             canvas?.drawCircle(circleX,circleY,50F,paint)
         }
 
+        //画面タッチされた時はonTouchEvent
         override fun onTouchEvent(event: MotionEvent?): Boolean {
-            //タッチポジション
+            //タッチポジションの代入、!!はnullを許容しない！
             circleX = event!!.x
             circleY = event!!.y
+            //無効化、毎回更新し続ける時は
             invalidate()
 
             //↓ return false この処理が終わってないので次の処理に行く、次の処理はないのでおかしくなる
